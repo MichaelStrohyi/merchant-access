161d6eeb3c0df312f0d5840cee8ee31b9fc2f178
Michael Strohyi <michael.strohyi@gmail.com>
2016-06-30 23:51:16 +0300

    [Core]store_delete: added

--- www/accounts/store_delete.php
+++ www/accounts/store_delete.php
@@ -1,20 +1,50 @@
 <?php
 
 require_once __DIR__  . '/../../include/core.php';
+require_once INCLUDE_DIR  . 'customers.php';
 
 $store_id = filter_input(INPUT_GET, 'store');
+$button_submit = filter_input(INPUT_POST, 'buttonSubmit');
+$button_cancel = filter_input(INPUT_POST, 'buttonCancel');
+
+if (isset($button_cancel)) {
+    redirectToPage('stores');
+    exit;
+}
+
 $customer = getLoggedCustomer();
 
 $store = new App\Store($customer, $store_id);
 
 if (!$store->exists()) {
-    echo "You have no permission to work with this store";
+    echo $twig->render('Panel/store-access-denied.html.twig', [
+        'message' => 'You have no permission to work with this store',
+        'url' => getPath('stores'),
+        ]);
     exit;
 }
 
-if (!$store->isActive()) {
-    echo "You have not validated this store ownership yet. Please, validate it.";
+if (!$store->isActive()) {    
+    echo $twig->render('Panel/store-access-denied.html.twig', [
+        'message' => 'You have not validated this store ownership yet. Please, validate it or <a href="' . getPath('resend_verification', ['store' => $store]) . '">click here</a> to request new validation email',
+        'url' => getPath('stores'),
+        ]);
     exit;
 }
 
-echo "Congratulations, you can delete this store";
+if (isset($button_submit)) {
+    $store_name = $store->getName();
+
+    emailConfirmation('store_rm_verification', ['store' => $store]);
+
+     # display success store deleting page
+    echo $twig->render('Panel/success-store-rm.html.twig', [
+        'store' => $store,
+        ]);
+    exit;
+}
+
+echo $twig->render('Panel/store-rm.html.twig', [
+    'store' => $store
+    ]);
+
