4011d47d8c745c0d29c31e9b67cc96541d61dd57
Michael Strohyi <michael.strohyi@gmail.com>
2016-07-15 15:11:15 +0300

    [Core]stores: added urls for Activate and Verify removing buttons

--- www/stores/stores.php
+++ www/stores/stores.php
@@ -11,6 +11,8 @@ $stores = $customer->getStores();
 #render page with list of customer's stores
 $routes = ['store_info' => getPath('store_info'),
     'store_delete' => getPath('store_rm'),
+    'store_activate' => getPath('resend_verification'),
+    'store_verify_delete' => getPath('resend_rm_verification'),
     'store_coupons' => getPath('store_coupons'),
     'store_add' => getPath('store_add'),
     ];
