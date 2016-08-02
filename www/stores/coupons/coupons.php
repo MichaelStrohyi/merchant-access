5c0aa41453aca6042e47387028e3d3ee5e1c1db0
Michael Strohyi <michael.strohyi@gmail.com>
2016-08-02 15:02:33 +0300

    [Core]coupons: added code to validate form data and save it

--- www/stores/coupons/coupons.php
+++ www/stores/coupons/coupons.php
@@ -35,27 +35,48 @@ if (isset($buttons_data['buttonCancel'])) {
     exit;
 }
 
-// !!! stub
-$new_image[10] = ['errorString' => 'Some error for 1st image',
-    'hasError' => true,
-    ];
-// !!! endof stub
-
 # if button Save is pressed
 if (isset($buttons_data['buttonSave']))
     {
     # grab data from $coupons_data to $coupons vars
     $coupons = $store->getCoupons();
+    $no_error = true;
+
     foreach ($coupons_data as $coupon_id => $value) {
         $coupons[$coupon_id]->fetchInfo($value);
         $coupons[$coupon_id]->validate();
+        if (!$coupons[$coupon_id]->isValid()) {
+            $no_error = false;
+        }
     }
+
+    if ($no_error) {
+        # save coupons into db
+        if (!$store->saveCoupons()) {
+            # if logo was not deleted create error message depending on working mode
+            if (!(defined('ENVIRONMENT') && ENVIRONMENT == 'development')) {
+                $message = 'Some arror happens. Please, try again later.';
+            } else {
+                $message = '<table border=3><tr><td bgcolor="yellow">Error happens trying to save store coupons</tr></td></table>';
+            }
+
+            # display error page
+            echo $twig->render('Signup/db-access-error.html.twig', [
+                'customer' => $store->getCustomer(),
+                'message' => $message,
+                ]);
+            exit;
+        }
+
+        $message = 'saved';
+    }
+
 }
+
 
 # display coupons page
 echo $twig->render('Coupons/coupons-list.html.twig', [
     'store' => $store,
     'url' => getPath('image'),
     'message' => $message,
-    'new_image' => $new_image,
     ]);
