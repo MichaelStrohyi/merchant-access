0b1c3a62bbb5f77308b12bbf783252406005acbf
Michael Strohyi <michael.strohyi@gmail.com>
2016-12-16 13:12:40 +0200

    [Core]main-page: added redirect to stores page after login

--- www/accounts/main.php
+++ www/accounts/main.php
@@ -3,5 +3,7 @@
 require_once __DIR__  . '/../../include/core.php';
 require_once __DIR__  . '/../../include/routes.php';
 
+redirectToPage('stores');
+
 echo 'Hello ' . $customer->getName() . '<br> You can see list of your stores <a href="' . getPath('stores') . '">here</a>';
 
